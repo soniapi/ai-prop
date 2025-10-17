e81ce13f9e681ffa4f0464d43e0254450a96894f
Sonia Pignorel <396009+soniapi@users.noreply.github.com>
2025-10-17 12:47:45 -0700

    feat(lib): add proportion hypothesis framework functions e.g. proportions, pooled estimate, z-statistics

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,28 @@ pub fn divider(
         .expect("Partition can't be created");
     
 }
+
+
+pub fn calculate_proportions (
+    m: &f32, m1: &f32, m2: &f32, n: &f32, n1: &f32, n2: &f32,
+) -> (f32, f32, f32) {
+    println!("Numbers m {:?}, m1: {:?}, m2: {:?}, n: {:?}, n1: {:?}, n2: {:?}", m, m1, m2, n, n1, n2);
+    let p1: f32 = m1 / n1;
+    let p2: f32 = m2 / n2;
+    let p_population: f32 = m / n;
+    println!("Proportion for whole population {:?}, population 1 {:?}, population 2 {:?}", p_population, p1, p2);
+    (p_population, p1, p2)
+}
+
+pub fn calculate_pooled_estimate (n1: &f32, n2: &f32, p1: &f32, p2: &f32) -> f32 {
+    let p = ((n1 * p1) + (n2 * p2)) / (n1 + n2);
+    println!("Pooled estimate: {:?}", p);
+    p
+}
+
+pub fn calculate_z_statistics (n1: &f32, n2: &f32, p1: &f32, p2: &f32, pooled_estimate: &f32) -> f32 {
+    let to_be_sqrt = (pooled_estimate * (1.0 - pooled_estimate))*((1.0 / n1) +( 1.0 / n2));
+    let z = (p1 - p2) / (to_be_sqrt as f32).sqrt();
+    println!("Z statistics: {:?}", z);
+    z
+}
