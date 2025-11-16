1865f35c2cd352734d4244b83456ef52b219b98c
Sonia Pignorel <396009+soniapi@users.noreply.github.com>
2025-11-16 12:42:27 -0800

    feat(populations): update runtime value for dividing variable

--- src/bin/divide_population_by_s.rs
+++ src/bin/divide_population_by_s.rs
@@ -2,6 +2,6 @@ use infra::{establish_connection, fill_partitions};
 
 fn main() {
     let connection = &mut establish_connection();
-    prop::divider(connection, &100.00);
+    prop::divider(connection, &100000.00);
     fill_partitions();
 }
